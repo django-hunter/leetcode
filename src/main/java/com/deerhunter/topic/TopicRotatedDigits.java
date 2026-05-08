b7881acdf28b5a9cc06c03a20728d1560d1ec281
django <deerhunter0837@gmail.com>
2026-05-08 22:58:04 +0800

    fix: 修复错误

--- src/main/java/com/deerhunter/topic/TopicRotatedDigits.java
+++ src/main/java/com/deerhunter/topic/TopicRotatedDigits.java
@@ -8,30 +8,32 @@ package com.deerhunter.topic;
  */
 public class TopicRotatedDigits {
     public static class Solution1 {
-        private static final int[] rotated=new int[]{0,1,5,-1,-1,2,9,-1,8,6};
+        private static final int[] rotated = new int[] {0, 1, 5, -1, -1, 2, 9, -1, 8, 6};
+
         public int rotatedDigits(int n) {
-            int cnt=0;
-            for(int i=1;i<=n;i++){
-                if(canRotate(i)){
+            int cnt = 0;
+            for (int i = 1; i <= n; i++) {
+                if (canRotate(i)) {
                     cnt++;
                 }
             }
             return cnt;
         }
 
-        private boolean canRotate(int n){
-            int result=0;
-            int curr=n;
-            while(curr>0){
-                result*=10;
-                int digit=rotated[curr%10];
-                if(digit==-1){
+        private boolean canRotate(int n) {
+            int result = 0;
+            int curr = n;
+            int base = 1;
+            while (curr > 0) {
+                int digit = rotated[curr % 10];
+                if (digit == -1) {
                     return false;
                 }
-                result+=digit;
-                curr/=10;
+                result += base * digit;
+                curr /= 10;
+                base *= 10;
             }
-            return result!=n;
+            return result != n;
         }
     }
 }
--- src/test/java/com/deerhunter/topic/TopicRotatedDigitsTest.java
+++ src/test/java/com/deerhunter/topic/TopicRotatedDigitsTest.java
@@ -5,16 +5,16 @@ import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.Test;
 
 /**
- * Oops, forgot to write comments. Good luck, bro.
+ * <a href="https://leetcode.cn/submissions/detail/723646981/"></a>
  *
  * @author chenxuejun
  * @since 2026/5/2 17:49
  */
 class TopicRotatedDigitsTest {
-  @Test
-  void solution1() {
-    TopicRotatedDigits.Solution1 solution = new TopicRotatedDigits.Solution1();
-    int cnt = solution.rotatedDigits(10);
-    assertThat(cnt).isEqualTo(4);
-  }
+    @Test
+    void solution1() {
+        TopicRotatedDigits.Solution1 solution = new TopicRotatedDigits.Solution1();
+        int cnt = solution.rotatedDigits(10);
+        assertThat(cnt).isEqualTo(4);
+    }
 }
